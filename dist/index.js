3ac43d24a678a620a4124b1f1c919b1eb1c76f1a
augustin <azzerty23@hotmail.fr>
2022-05-04 15:04:21 +0200

    chore: init project

---
+++ dist/index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function hello(world = "world") {
+    return `Hello ${world}!`;
+}
+exports.default = hello;
+console.log(hello("you"));
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+export default function hello(world: string = "world"): string {
+  return `Hello ${world}!`;
+}
+
+console.log(hello("you"));
